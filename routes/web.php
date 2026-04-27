3b3cb6dc8baf571824efb1e2917f4e5a5e3c83c1
Shivadarsh <shivadarsh777@gmail.com>
2026-04-27 17:25:36 +0530

    deleted welcome.blade.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Adoremom API is running',
+        'status' => 'healthy',
+        'version' => '1.0.0'
+    ]);
 });
